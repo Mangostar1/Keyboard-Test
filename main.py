ba04df39c89a02ac142f02191892ffa7a47b4f9a
Mangostar1 <influencia.x.94@gmail.com>
2026-07-29 16:55:49 -0400

    Se añaden informacin extra y el esqueleto de lo que seran las futuras implementaciones

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import src.typing_speed_test as speed
+import src.detector as detector
 
 while True:
     
     print("Bienvenido al Keyboard Tests")
     print("1. Medir velocidad de escritura")
+    print("2. Detectar chatter")
+    print("3. Test de teclas")
+    print("4. Estadísticas")
     print("5. Salir")
     opcion = input("Seleccione una opción: ")
 
     if opcion == "1": #mide la velocidad a la que escribe el usuario
         speed.medir_velocidad_escritura()
 
-    elif opcion == "2":
-        #detectar_chatter()
+    elif opcion == "2": #detecta si el usuario tiene chatter
+        detector.detectar_chatter()
         break
 
     elif opcion == "3":
@@ -27,3 +31,14 @@ while True:
     
     else:
         print("Opción inválida. Por favor, seleccione una opción válida.")
+        
+        
+""" 
+
+DICCIONARIO DE FUNCIONES:
+1. medir_velocidad_escritura(): Mide la velocidad de escritura del usuario.
+2. detectar_chatter(): Detecta si el usuario tiene chatter en su teclado.
+3. test_teclas(): Realiza un test de teclas (función pendiente de implementación).
+4. estadisticas(): Muestra estadísticas relacionadas con el uso del teclado (función pendiente de implementación).
+
+"""
--- src/detector.py
+++ src/detector.py
@@ -1,4 +1,19 @@
 import keyboard
 import time
 
-# chatter 1
+def detectar_chatter():
+  print("key chatter en construccion...")
+  print("Presione ESC para salir.")
+  
+  while True:
+    
+    evento = keyboard.read_event()
+    
+    if evento.event_type == keyboard.KEY_DOWN:
+      
+      print("Funcion de deteccion de chatter en construccion...")
+    
+      if evento.name == "esc":
+        
+        print("Saliendo...")
+        break
