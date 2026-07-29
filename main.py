818ae9e3032e5f75b19207c1dba4b927ac361b3c
Mangostar1 <influencia.x.94@gmail.com>
2026-07-28 20:21:05 -0400

    Se guarda progreso en el keyboard test

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import src.typing_speed_test as speed
 
-print("Bienvenido al Keyboard Tests")
-print("Presione una opcion:")
-print("1. Medir velocidad del teclado")
-print("5. Salir")
-
 while True:
-
+    
+    print("Bienvenido al Keyboard Tests")
+    print("1. Medir velocidad de escritura")
+    print("5. Salir")
     opcion = input("Seleccione una opción: ")
 
-    if opcion == "1":
-        speed.medir_velocidad_teclado()
+    if opcion == "1": #mide la velocidad a la que escribe el usuario
+        speed.medir_velocidad_escritura()
 
     elif opcion == "2":
         #detectar_chatter()
@@ -24,5 +22,8 @@ while True:
         #estadisticas()
         break
 
-    elif opcion == "5":
+    elif opcion == "5" or opcion.lower() == "salir":
         break
+    
+    else:
+        print("Opción inválida. Por favor, seleccione una opción válida.")
--- src/typing_speed_test.py
+++ src/typing_speed_test.py
@@ -1,9 +1,12 @@
 import keyboard
 import time
 
-def medir_velocidad_teclado(umbral_ms=60):
-    ultima_tecla = {}
-    ultimo_tiempo = time.perf_counter()
+
+def medir_velocidad_escritura():
+
+    velocidad_usuario = 0 # velocidad por defecto
+    total_teclas = 0 # total de teclas presionadas
+    ultimo_tiempo = None # tiempo del último evento de teclado
 
     print("Presione ESC para salir.")
 
@@ -11,46 +14,21 @@ def medir_velocidad_teclado(umbral_ms=60):
         evento = keyboard.read_event()
 
         if evento.event_type == keyboard.KEY_DOWN:
+            
+            if total_teclas == 0: # se asigna el tiempo actual al primer evento de teclado
+                ultimo_tiempo = time.perf_counter() # se asigna el tiempo actual al primer evento de teclado
+                total_teclas += 1
+                continue
+            
             tiempo_actual = time.perf_counter()
-            diferencia = (tiempo_actual - ultimo_tiempo) * 1000
-
-            if diferencia < umbral_ms:
-                print("¡Tecla presionada demasiado rápido!")
+            velocidad_usuario = (tiempo_actual - ultimo_tiempo) * 1000 # se asigna la velocidad en milisegundos
+            total_teclas += 1
 
             ultimo_tiempo = tiempo_actual
+            
+            print(f"Velocidad de escritura: {velocidad_usuario:.2f} ms")
+            print(f"Total de teclas presionadas: {total_teclas}")
 
         if evento.name == "esc":
             print("Saliendo...")
             break
-
-
-
-
-""" umbral_ms = 60
-
-ultima_tecla = {}
-
-ultimo_tiempo = time.perf_counter()
-
-print("Presione ESC para salir.")
-
-while True:
-
-  evento = keyboard.read_event()
-
-  if evento.event_type == keyboard.KEY_DOWN:
-
-    tiempo_actual = time.perf_counter()
-
-    diferencia = (tiempo_actual - ultimo_tiempo) * 1000
-
-    # print(f"Tecla: {evento.name} ({diferencia:.2f} ms)")
-    
-    if diferencia < umbral_ms:
-      print("¡Tecla presionada demasiado rápido!")
-
-    ultimo_tiempo = tiempo_actual
-
-  if evento.name == "esc":
-    print("Saliendo...")
-    break """
